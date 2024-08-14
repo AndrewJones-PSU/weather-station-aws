3af6844390fac8a689fabf249be074296d5f1c62
AndrewJones-PSU <andrewjonesnoncorp@gmail.com>
2024-08-14 16:23:00 -0400

    formatter guh moment

--- lib/LambdaStack.ts
+++ lib/LambdaStack.ts
@@ -3,22 +3,22 @@ import { aws_lambda as lambda } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class LambdaStack extends cdk.Stack {
-    esp32_lambda: lambda.Function;
-    webserver_lambda: lambda.Function;
+	esp32_lambda: lambda.Function;
+	webserver_lambda: lambda.Function;
 
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-        this.esp32_lambda = new lambda.Function(this, "ESP32Lambda", {
-            runtime: lambda.Runtime.NODEJS_LATEST,
-            code: lambda.Code.fromAsset("resources"),
-            handler: "esp32_handler.main",
-        });
+		this.esp32_lambda = new lambda.Function(this, "ESP32Lambda", {
+			runtime: lambda.Runtime.NODEJS_LATEST,
+			code: lambda.Code.fromAsset("resources"),
+			handler: "esp32_handler.main",
+		});
 
-        this.webserver_lambda = new lambda.Function(this, "WebserverLambda", {
-            runtime: lambda.Runtime.NODEJS_LATEST,
-            code: lambda.Code.fromAsset("resources"),
-            handler: "webserver_handler.main",
-        });
-    }
+		this.webserver_lambda = new lambda.Function(this, "WebserverLambda", {
+			runtime: lambda.Runtime.NODEJS_LATEST,
+			code: lambda.Code.fromAsset("resources"),
+			handler: "webserver_handler.main",
+		});
+	}
 }
